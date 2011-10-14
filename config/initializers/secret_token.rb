db2c13b2a01718ec798d87960563f751a3ca3828
Cameron Voell <cameronvoell@gmail.com>
2011-10-13 22:57:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwinLights::Application.config.secret_token = 'fc9de94cc6eb1834e4df8ae46fae2726fe9dd8ce005d2e76a25ec7b92652d0fd212072ec958a6a7451e1383cfb24c9edf14ca17920922a8eef9dbe8409436744'
